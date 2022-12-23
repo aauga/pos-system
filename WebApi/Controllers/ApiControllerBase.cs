c7e373e00a25b19423aaaf7943ff95ed80a6e705
Adomas <adomas@benme.io>
2022-12-23 23:20:20 +0200

    Create helper function which returns 201 Created

--- WebApi/Controllers/ApiControllerBase.cs
+++ WebApi/Controllers/ApiControllerBase.cs
@@ -1,9 +1,10 @@
+using Domain.Common;
 using MediatR;
 
 using Microsoft.AspNetCore.Mvc;
 using WebAPI.Filters;
 
-namespace CleanArchitecture.WebUI.Controllers;
+namespace WebUI.Controllers;
 
 [ApiController]
 [Route("api/v1/[controller]")]
@@ -13,4 +14,19 @@ public abstract class ApiControllerBase : ControllerBase
     private ISender _mediator = null!;
 
     protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+
+    public CreatedResult Created(object? value)
+    {
+        var request = HttpContext.Request;
+        var id = (value as BaseEntity)?.Id;
+
+        var uri = string.Format("{0}://{1}{2}/{3}",
+            request.Scheme,
+            request.Host,
+            request.Path,
+            id
+        );
+
+        return Created(uri, value);
+    }
 }
