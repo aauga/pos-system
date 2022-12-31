f4729692f5cb4cbe8e3d09e2b73b310cf4473332
Adomas <adomas@benme.io>
2022-12-31 17:13:23 +0200

    Create migration which removes nullable fields

---
+++ Infrastructure/Persistence/Migrations/20221231151235_RemoveNullableOperators.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveNullableOperators : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Tenants_TenantId",
+                table: "Employees");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TenantId",
+                table: "Employees",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Tenants_TenantId",
+                table: "Employees",
+                column: "TenantId",
+                principalTable: "Tenants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Tenants_TenantId",
+                table: "Employees");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TenantId",
+                table: "Employees",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Tenants_TenantId",
+                table: "Employees",
+                column: "TenantId",
+                principalTable: "Tenants",
+                principalColumn: "Id");
+        }
+    }
+}
