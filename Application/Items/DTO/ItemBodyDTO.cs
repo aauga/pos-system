eeaf55c9b735f2dc98992fed3d777d9283ad38d1
audriussav <audriussav@wix.com>
2022-12-31 15:27:53 +0200

    change DTOs to record

--- Application/Items/DTO/ItemBodyDTO.cs
+++ Application/Items/DTO/ItemBodyDTO.cs
@@ -2,13 +2,13 @@
 
 namespace Application.Items;
 
-public class ItemBodyDto
+public record ItemBodyDto
 {
-    public string? Title { get; set; }
-    public string? Category { get; set; }
+    public string Title { get; set; }
+    public string Category { get; set; }
     public decimal Price { get; set; }
-    public string? Description { get; set; }
-    public string? Brand { get; set; }
-    public string? Photo { get; set; }
+    public string Description { get; set; }
+    public string Brand { get; set; }
+    public string Photo { get; set; }
     public int TenantId { get; set; }
 }
--- Application/Items/DTO/ItemDTO.cs
+++ Application/Items/DTO/ItemDTO.cs
@@ -2,15 +2,15 @@
 
 namespace Application.Items;
 
-public class ItemDto
+public record ItemDto
 {
     public int Id { get; set; }
-    public string? Title { get; set; }
-    public string? Category { get; set; }
+    public string Title { get; set; }
+    public string Category { get; set; }
     public decimal Price { get; set; }
-    public string? Description { get; set; }
-    public string? Brand { get; set; }
-    public string? Photo { get; set; }
+    public string Description { get; set; }
+    public string Brand { get; set; }
+    public string Photo { get; set; }
     public int TenantId { get; set; }
 }
 
