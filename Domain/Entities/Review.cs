62d9a04778bf4723ea66804995eec236cf588f28
audriussav <audriussav@wix.com>
2023-01-01 15:24:22 +0200

    remove Review nullables

--- Domain/Entities/Review.cs
+++ Domain/Entities/Review.cs
@@ -9,6 +9,6 @@ public class Review : BaseEntity
     public Employee Employee { get; set; }
     public int EmployeeId { get; set; }
     public int Rating { get; set; }
-    public String? Description { get; set; }
-    public String? Photo { get; set; }
+    public String Description { get; set; }
+    public String Photo { get; set; }
 }
